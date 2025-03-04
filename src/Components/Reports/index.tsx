6ed58ac5fcfcd44e5b343aa7cf3300b9a122b482
Evanansemprun <evanansemprun@gmail.com>
2025-03-03 22:25:33 -0400

    some fix in reports

--- src/Components/Reports/index.tsx
+++ src/Components/Reports/index.tsx
@@ -1,6 +1,8 @@
+import * as XLSX from 'xlsx';
 import { DatePicker } from '@mantine/dates';
+import { useMediaQuery } from '@mantine/hooks';
 import { useState, useEffect, SetStateAction } from 'react';
-import { IconAdjustments, IconCopy, IconEye, IconSearch } from '@tabler/icons-react';
+import { IconAdjustments, IconCopy, IconReload, IconEye, IconSearch, IconDownload } from '@tabler/icons-react';
 import { Button, Group, ScrollArea, Table, Text, Modal, Title, ActionIcon, Input, Pagination } from '@mantine/core';
 import { fetchUserRole, fetchReports, handleSearchChange, formatDate, handlePinClick, copyToClipboard } from '../../utils/utils';
 
@@ -9,6 +11,34 @@ interface ReportsProps {
 }
 
 function Reports({ user }: ReportsProps) {
+
+  const exportToExcel = (data: any[]) => {
+    const filteredData = data.map((report) => {
+      const { user, order_id, _id, product, created_at, __v, status, ...cleanedReport } = report;
+
+      const formattedDate = new Date(report.created_at);
+      const formattedDateStr = `${formattedDate.getDate().toString().padStart(2, '0')}/${(formattedDate.getMonth() + 1).toString().padStart(2, '0')}/${formattedDate.getFullYear()} ${formattedDate.getHours().toString().padStart(2, '0')}:${formattedDate.getMinutes().toString().padStart(2, '0')}`;
+
+      cleanedReport['Fecha'] = formattedDateStr;
+
+      if (userRole === 'master') {
+        cleanedReport['Usuario'] = report.user.handle;
+      }
+
+      return cleanedReport;
+    });
+
+    const wb = XLSX.utils.book_new();
+
+    const ws = XLSX.utils.json_to_sheet(filteredData);
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Reportes');
+
+    XLSX.writeFile(wb, 'reportes_ventas.xlsx');
+  };
+
+
+
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [finishDate, setFinishDate] = useState<Date | null>(null);
   const [filteredReports, setFilteredReports] = useState<any[]>([]);
@@ -22,6 +52,7 @@ function Reports({ user }: ReportsProps) {
   const [searchQuery, setSearchQuery] = useState('');
   const itemsPerPage = 7;
 
+  const isMobile = useMediaQuery('(max-width: 1000px)');
   const userId = user?._id || null;
 
   useEffect(() => {
@@ -38,9 +69,33 @@ function Reports({ user }: ReportsProps) {
 
   useEffect(() => {
     if (allReports.length > 0) {
-      handleSearchChange(searchQuery, allReports, setFilteredReports);
+      handleSearchChange(searchQuery, filteredReports.length > 0 ? filteredReports : allReports, setFilteredReports);
     }
-  }, [searchQuery, allReports]);
+  }, [searchQuery, allReports, filteredReports]);
+
+  const handleFilterDates = () => {
+    if (startDate && finishDate) {
+      const filtered = allReports.filter((report) => {
+        const reportDate = new Date(report.created_at);
+        const finishDateEndOfDay = new Date(finishDate);
+        finishDateEndOfDay.setHours(23, 59, 59, 999);
+
+        return reportDate >= startDate && reportDate <= finishDateEndOfDay;
+      });
+      setFilteredReports(filtered);
+    }
+    setOpened(false);
+  };
+
+  const clearDateFilter = () => {
+    setStartDate(null);
+    setFinishDate(null);
+    setFilteredReports(allReports);
+  };
+
+  useEffect(() => {
+    handleSearchChange(searchQuery, filteredReports.length > 0 ? filteredReports : allReports, setFilteredReports);
+  }, [searchQuery, filteredReports]);
 
   const paginatedReports = filteredReports.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
   const totalPages = Math.ceil(filteredReports.length / itemsPerPage);
@@ -48,10 +103,17 @@ function Reports({ user }: ReportsProps) {
     setCurrentPage(page);
   };
 
+  useEffect(() => {
+    if (opened) {
+      setStartDate(null);
+      setFinishDate(null);
+    }
+  }, [opened]);
+
   return (
-    <div style={{ padding: '1rem', maxWidth: '1200px', margin: '0 auto', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-      <Modal radius='lg' withCloseButton={false} opened={pinsModalOpened} onClose={() => setPinsModalOpened(false)}>
-        <ScrollArea style={{ height: '300px', width: '100%' }}>
+    <>
+      <Modal radius="lg" withCloseButton={false} opened={pinsModalOpened} onClose={() => setPinsModalOpened(false)}>
+        <ScrollArea style={{ height: '350px', width: '100%' }}>
           <Table striped highlightOnHover withColumnBorders>
             <thead>
               <tr>
@@ -76,51 +138,104 @@ function Reports({ user }: ReportsProps) {
           </Table>
         </ScrollArea>
       </Modal>
-      <Modal radius='lg' withCloseButton={false} opened={opened} onClose={() => setOpened(false)}>
-        <Title ta="center" weight={700} mb="md" order={2}>Filtrar por fecha</Title>
-        <Group mb="md" style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem', justifyContent: 'center' }}>
-          <DatePicker placeholder="Fecha de inicio" label="Inicio" value={startDate} radius="md" size="lg" onChange={setStartDate} />
-          <DatePicker placeholder="Fecha de fin" label="Fin" value={finishDate} radius="md" size="lg" onChange={setFinishDate} />
-        </Group>
-        <Group position="center" mb="md">
-          <Button radius="lg" className="button" size="lg">Filtrar Reportes</Button>
-        </Group>
-      </Modal>
-      <Title ta="center" weight={700} mb="md" order={2}>
-        Reportes de Ventas
-      </Title>
 
-      <Group position='apart'>
-        <Pagination
-          total={totalPages}
-          radius="md"
-          size="lg"
-          position="center"
-          page={currentPage}
-          onChange={handlePageChange}
-          styles={(theme) => ({
-            item: {
-              '&[data-active]': {
-                backgroundImage: theme.fn.gradient({ from: '#0c2a85', to: '#0c2a85' }),
-              },
-            },
-          })}
-        />
-        <Group>
-          <Input
+      <Modal radius="lg" withCloseButton={false} opened={opened} onClose={() => setOpened(false)}>
+
+        <Title ta="center" weight={700} mb="md" order={2}>Filtrar por fecha</Title>
+
+        <Group
+          mt={15}
+          mb={15}
+          style={{
+            display: 'grid',
+            gridTemplateColumns: isMobile ? '1fr' : '1fr 1fr',
+            gap: '10px',
+            width: '100%',
+          }}
+        >
+          <DatePicker
+            placeholder="Fecha de inicio"
+            label="Inicio"
+            value={startDate}
             radius="md"
             size="lg"
-            icon={<IconSearch />}
-            placeholder="Producto o Usuario"
-            value={searchQuery}
-            onChange={(e: { target: { value: SetStateAction<string>; }; }) => setSearchQuery(e.target.value)} // Actualizar `searchQuery`
+            onChange={setStartDate}
           />
-          <ActionIcon style={{ background: '#0c2a85', color: 'white', marginLeft: '10px' }} radius="md" size="xl" onClick={() => setOpened(true)} color="indigo" variant="filled">
+          <DatePicker
+            placeholder="Fecha de fin"
+            label="Fin"
+            value={finishDate}
+            radius="md"
+            size="lg"
+            onChange={setFinishDate}
+          />
+        </Group>
+
+
+        <Group position="center" mb="md">
+          <Button radius="lg" className="button" size="lg" onClick={handleFilterDates}>Filtrar Reportes</Button>
+        </Group>
+
+      </Modal>
+      <Title ta="center" weight={700} mb="sm" order={2}>Reportes de Ventas</Title>
+
+      <Group
+        style={{
+          display: 'grid',
+          gridTemplateColumns: isMobile ? '1fr' : ' 4fr 1fr',
+          gap: '10px',
+          width: '100%',
+        }}
+      >
+
+        <Input
+          radius="md"
+          size="lg"
+          icon={<IconSearch />}
+          placeholder="Producto o Usuario"
+          value={searchQuery}
+          onChange={(e: { target: { value: SetStateAction<string> } }) => setSearchQuery(e.target.value)}
+        />
+        <Group>
+          <ActionIcon style={{ background: '#0c2a85', color: 'white', }} radius="md" size="xl" onClick={() => setOpened(true)} color="indigo" variant="filled">
             <IconAdjustments size={30} />
           </ActionIcon>
+          <ActionIcon
+            style={{ background: '#0c2a85', color: 'white', }} radius="md" size="xl"
+            color="indigo"
+            variant="filled"
+            onClick={clearDateFilter}
+          >
+            <IconReload size={30} />
+          </ActionIcon>
+          <ActionIcon
+            style={{ background: '#0c2a85', color: 'white', }}
+            radius="md"
+            size="xl"
+            color="indigo"
+            variant="filled"
+            onClick={() => exportToExcel(filteredReports)}
+          >
+            <IconDownload size={30} />
+          </ActionIcon>
         </Group>
-      </Group>
 
+      </Group>
+      <Pagination
+        total={totalPages}
+        radius="md"
+        mt={15}
+        size="lg"
+        page={currentPage}
+        onChange={handlePageChange}
+        styles={(theme) => ({
+          item: {
+            '&[data-active]': {
+              backgroundImage: theme.fn.gradient({ from: '#0c2a85', to: '#0c2a85' }),
+            },
+          },
+        })}
+      />
       {error && <Text color="red" mb="md">{error}</Text>}
 
       {filteredReports.length === 0 && !error && (
@@ -130,15 +245,17 @@ function Reports({ user }: ReportsProps) {
       )}
 
       {filteredReports.length > 0 && (
-        <ScrollArea style={{ height: '342px', width: '100%' }} type="never">
-          <Table striped highlightOnHover withBorder withColumnBorders>
+        <ScrollArea style={{ height: '420px', width: '100%' }} type="never">
+          <Table mt={15} mb={isMobile ? 100 : 5} striped highlightOnHover withBorder withColumnBorders>
             <thead style={{ background: '#0c2a85' }}>
               <tr>
-                <th style={{ textAlign: 'center',color:'white' }}><Title order={3}>Producto</Title></th>
-                <th style={{ textAlign: 'center',color:'white' }}><Title order={3}>Precio</Title></th>
-                <th style={{ textAlign: 'center' ,color:'white'}}><Title order={3}>Cantidad</Title></th>
-                <th style={{ textAlign: 'center',color:'white' }}><Title order={3}>Fecha</Title></th>
-                <th style={{ textAlign: 'center',color:'white' }}><Title order={3}>Usuarios</Title></th>
+                <th style={{ textAlign: 'center', color: 'white' }}><Title order={3}>Producto</Title></th>
+                <th style={{ textAlign: 'center', color: 'white' }}><Title order={3}>Precio</Title></th>
+                <th style={{ textAlign: 'center', color: 'white' }}><Title order={3}>Cantidad</Title></th>
+                <th style={{ textAlign: 'center', color: 'white' }}><Title order={3}>Fecha</Title></th>
+                {userRole !== 'cliente' && userRole !== 'vendedor' && (
+                  <th style={{ textAlign: 'center', color: 'white' }}><Title order={3}>Usuarios</Title></th>
+                )}
                 <th></th>
               </tr>
             </thead>
@@ -150,7 +267,9 @@ function Reports({ user }: ReportsProps) {
                   <td style={{ textAlign: 'center' }}>{report.totalPrice}$</td>
                   <td style={{ textAlign: 'center' }}>{report.quantity}</td>
                   <td style={{ textAlign: 'center' }}>{formatDate(report.created_at)}</td>
-                  <td style={{ textAlign: 'center' }}>{report.user.handle}</td>
+                  {userRole !== 'cliente' && userRole !== 'vendedor' && (
+                    <td style={{ textAlign: 'center' }}>{report.user.handle}</td>
+                  )}
                   <td style={{ display: 'flex', justifyContent: 'center' }}>
                     <ActionIcon
                       style={{ background: '#0c2a85', color: 'white', marginLeft: '10px' }}
@@ -167,7 +286,7 @@ function Reports({ user }: ReportsProps) {
           </Table>
         </ScrollArea>
       )}
-    </div>
+    </>
   );
 }
 
