4cb83dc65873ce7b86706bdea219a273a49d5e35
Evanansemprun <evanansemprun@gmail.com>
2025-03-06 12:33:40 -0400

    balance fix

--- src/Components/BalanceReports/index.tsx
+++ src/Components/BalanceReports/index.tsx
@@ -3,8 +3,8 @@ import { DatePicker } from '@mantine/dates';
 import { useMediaQuery } from '@mantine/hooks';
 import { fetchTransactions } from '../../utils/utils';
 import { useState, useEffect } from 'react';
-import { IconCalendarWeek, IconDownload, IconReload, IconUser } from '@tabler/icons-react';
-import { Group, Table, Text, Title, ActionIcon, Pagination, Select } from '@mantine/core';
+import { Group, Table, Text, Title, ActionIcon, Pagination, Select, Modal } from '@mantine/core';
+import { IconCalendarWeek, IconDownload, IconInfoCircle, IconReload, IconUser } from '@tabler/icons-react';
 
 
 const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
@@ -18,8 +18,14 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
     const itemsPerPage = 7;
     const userRole = user?.role;
-
+    const [opened, setOpened] = useState(false);
+    const [modalTransaction, setModalTransaction] = useState<any>(null);
     const isMobile = useMediaQuery('(max-width: 1000px)');
+
+    const openModal = (transaction: any) => {
+        setModalTransaction(transaction);
+        setOpened(true);
+    };
 
     useEffect(() => {
         if (user) {
@@ -40,13 +46,13 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
 
     useEffect(() => {
         let filtered = allTransactions;
-
+    
         if (selectedUserHandle && selectedUserHandle !== 'todos') {
             filtered = filtered.filter((transaction) =>
                 transaction.transactionUserName.toLowerCase().includes(selectedUserHandle.toLowerCase())
             );
         }
-
+    
         if (selectedDate) {
             const dateStr = selectedDate.toISOString().split('T')[0];
             filtered = filtered.filter(transaction => {
@@ -54,46 +60,79 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
                 return transactionDate === dateStr;
             });
         }
-
+    
         if (searchTerm) {
             filtered = filtered.filter((transaction) =>
                 transaction.transactionUserName?.toLowerCase().includes(searchTerm.toLowerCase())
             );
         }
-
-        setFilteredTransactions(filtered);
-        setCurrentPage(1);
+    
+        if (filtered !== filteredTransactions) {
+            setFilteredTransactions(filtered);
+            setCurrentPage(1);
+        }
     }, [selectedUserHandle, selectedDate, searchTerm, allTransactions]);
+    
 
     const exportToExcel = (data: any[]) => {
         const filteredData = data.map((transaction) => {
-            const { 
-                _id, 
-                userId, 
-                created_at, 
-                transactionUserName, 
-                userName, 
-                userEmail, 
-                userRole, 
-                userRango, 
-                userhandle, 
-                __v, 
-                user, 
-                ...cleanedTransaction 
+            const {
+                amount,          // Excluimos estas columnas
+                previousBalance, // Excluimos estas columnas
+                type,            // Excluimos estas columnas
+                transactionId,   // Excluimos estas columnas
+                _id,
+                userId,
+                created_at,
+                transactionUserName,
+                userName,
+                userEmail,
+                userRole,
+                userRango,
+                userhandle,
+                __v,
+                user,
+                ...cleanedTransaction
             } = transaction;
-    
+
             const formattedDate = new Date(transaction.created_at);
-            cleanedTransaction['Fecha'] = `${formattedDate.getDate().toString().padStart(2, '0')}/${(formattedDate.getMonth() + 1).toString().padStart(2, '0')}/${formattedDate.getFullYear()} ${formattedDate.getHours().toString().padStart(2, '0')}:${formattedDate.getMinutes().toString().padStart(2, '0')}`;
-            
+            const formattedDateStr = `${formattedDate.getDate().toString().padStart(2, '0')}/${(formattedDate.getMonth() + 1).toString().padStart(2, '0')}/${formattedDate.getFullYear()} ${formattedDate.getHours().toString().padStart(2, '0')}:${formattedDate.getMinutes().toString().padStart(2, '0')}`;
+
+            cleanedTransaction['ID'] = transaction.transactionId;
+            cleanedTransaction['Fecha'] = formattedDateStr;
+            cleanedTransaction['Saldo Anterior'] = `${transaction.previousBalance} USD`;
+            cleanedTransaction['Monto'] = `${transaction.amount} USD`;
+            cleanedTransaction['Saldo Final'] = `${transaction.amount + transaction.previousBalance} USD`;
+
+            if (userRole !== 'cliente') {
+                cleanedTransaction['Realizado por'] = transaction.transactionUserName;
+                cleanedTransaction['Beneficiado'] = transaction.userhandle;
+            }
+
             return cleanedTransaction;
         });
-    
+
         const wb = XLSX.utils.book_new();
         const ws = XLSX.utils.json_to_sheet(filteredData);
         XLSX.utils.book_append_sheet(wb, ws, 'Transacciones');
-        XLSX.writeFile(wb, 'balance_reports.xlsx');
+
+        let fileName = 'balance_reports';
+
+        if (selectedUserHandle && selectedUserHandle !== 'todos') {
+            fileName += `_${selectedUserHandle}`;
+        }
+
+        if (selectedDate) {
+            const dateStr = selectedDate.toISOString().split('T')[0];
+            fileName += `_${dateStr}`;
+        }
+
+        fileName += '.xlsx';
+
+        XLSX.writeFile(wb, fileName);
     };
-    
+
+
     const clearFilters = () => {
         setSelectedUserHandle('todos');
         setSelectedDate(null);
@@ -106,7 +145,7 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
 
     return (
         <>
-            <Title ta="center" weight={700} mb="sm" order={2}>Reportes de Balance</Title>
+            <Title ta="center" weight={700} mb="sm" order={2}>Reporte Carga De Saldo</Title>
             {userRole !== 'cliente' && userRole !== 'vendedor' && (
                 <>
 
@@ -154,8 +193,8 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
                             value={selectedDate}
                             onChange={setSelectedDate}
                         />
-                       
-            <Group position={isMobile ? 'center' : 'apart'} mt={25}>
+
+                        <Group position={isMobile ? 'center' : 'apart'} mt={25}>
                             <ActionIcon
                                 style={{ background: '#0c2a85', color: 'white', }} radius="md" size="xl"
                                 color="indigo"
@@ -179,9 +218,61 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
                 </>
             )}
 
+                    <Modal
+                        opened={opened}
+                        withCloseButton={false}
+                        onClose={() => setOpened(false)}
+                    >
+                        {modalTransaction && (
+                            <>
+                                <Title ta='center' mb={5} order={4}>Detalles de la recarga</Title>
+                                <Group position="center">
+                                    <IconUser size={150} />
+                                </Group>
+                                <Group mt='md' position="apart" mb="md">
+                                    <Title order={4}>ID:</Title>
+                                    <Title order={4}>{modalTransaction.transactionId}</Title>
+                                </Group>
+                                <Group mt='md' position="apart" mb="md">
+                                    <Title order={4}>Fecha:</Title>
+                                    <Title order={4}>{new Date(modalTransaction.created_at).toLocaleString('es-ES', {
+                                        year: 'numeric',
+                                        month: '2-digit',
+                                        day: '2-digit',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                    })}</Title>
+                                </Group>
+                                <Group mt='md' position="apart" mb="md">
+                                    <Title order={4}>Saldo Anterior:</Title>
+                                    <Title order={4}>{modalTransaction.previousBalance} USD</Title>
+                                </Group>
+                                <Group mt='md' position="apart" mb="md">
+                                    <Title order={4}>Carga de Saldo:</Title>
+                                    <Title order={4}>{modalTransaction.amount} USD USD</Title>
+                                </Group>
+                                <Group mt='md' position="apart" mb="md">
+                                    <Title order={4}>Saldo Final:</Title>
+                                    <Title order={4}>{modalTransaction.amount + modalTransaction.previousBalance} USD</Title>
+                                </Group>
+                                {userRole !== 'cliente' && (
+                                    <>
+                                        <Group mt='md' position="apart" mb="md">
+                                            <Title order={4}>Realizado por:</Title>
+                                            <Title order={4}>{modalTransaction.transactionUserName}</Title>
+                                        </Group>
+                                        <Group mt='md' position="apart" mb="md">
+                                            <Title order={4}>Beneficiado:</Title>
+                                            <Title order={4}>{modalTransaction.userhandle}</Title>
+                                        </Group>
+                                    </>
+                                )}
+
+                            </>
+                        )}
+                    </Modal>
             {userRole == 'cliente' && (
                 <>
-
                     <Group
                         style={{
                             display: 'grid',
@@ -241,38 +332,76 @@ const BalanceReports: React.FC<{ user: any }> = ({ user }) => {
             )}
 
             {filteredTransactions.length > 0 && (
-                    <Table mt={15} striped highlightOnHover withBorder withColumnBorders>
-                        <thead style={{ background: '#0c2a85' }}>
-                            <tr>
-                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>ID</Title></th>
-                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Fecha</Title></th>
-                                {userRole !== 'cliente' && (
+                <Table mt={15} striped highlightOnHover withBorder withColumnBorders>
+                    <thead style={{ background: '#0c2a85' }}>
+                        <tr>
+                            <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>ID</Title></th>
+                            <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Fecha</Title></th>
+                            {!isMobile && userRole !== 'cliente' && (
+                                <>
+                                    <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Realizado por</Title></th>
+                                    <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Beneficiado</Title></th>
+                                </>
+                            )}
+                            {!isMobile && (
+                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Saldo Anterior</Title></th>
+                            )}
+                            <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Carga de saldo</Title></th>
+                            {!isMobile && (
+                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Saldo Final</Title></th>
+                            )}
+                            {isMobile && (
+                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Info</Title></th>
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {paginatedReports.map((transaction) => (
+                            <tr key={transaction._id}>
+                                <td style={{ textAlign: 'center' }}>{transaction.transactionId}</td>
+                                <td style={{ textAlign: 'center' }}>
+                                    {new Date(transaction.created_at).toLocaleString('es-ES', {
+                                        year: 'numeric',
+                                        month: '2-digit',
+                                        day: '2-digit',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                    })}
+                                </td>
+
+                                {!isMobile && userRole !== 'cliente' && (
                                     <>
-                                        <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Realizado por</Title></th>
-                                        <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Beneficiado</Title></th>
+                                        <td style={{ textAlign: 'center' }}>{transaction.transactionUserName}</td>
+                                        <td style={{ textAlign: 'center' }}>{transaction.userhandle}</td>
                                     </>
                                 )}
-                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Monto</Title></th>
-                                <th style={{ textAlign: 'center', color: 'white' }}><Title order={4}>Balance Anterior</Title></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {paginatedReports.map((transaction) => (
-                                <tr key={transaction._id}>
-                                    <td style={{ textAlign: 'center' }}>{transaction.transactionId}</td>
-                                    <td style={{ textAlign: 'center' }}>{new Date(transaction.created_at).toLocaleDateString('es-ES')}</td>
-                                    {userRole !== 'cliente' && (
-                                        <>
-                                            <td style={{ textAlign: 'center' }}>{transaction.transactionUserName}</td>
-                                            <td style={{ textAlign: 'center' }}>{transaction.userhandle}</td>
-                                        </>
-                                    )}
-                                    <td style={{ textAlign: 'center' }}>{transaction.amount} USD</td>
+                                {!isMobile && (
                                     <td style={{ textAlign: 'center' }}>{transaction.previousBalance} USD</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
+                                )}
+                                <td style={{ textAlign: 'center' }}>{transaction.amount} USD</td>
+                                {!isMobile && (
+                                    <td style={{ textAlign: 'center' }}>{transaction.amount + transaction.previousBalance} USD</td>
+                                )}
+                                {isMobile && (
+
+                                    <td style={{ textAlign: 'center' }}>
+                                        <ActionIcon
+                                            style={{ background: '#0c2a85', color: 'white' }}
+                                            size="xl"
+                                            variant="filled"
+                                            onClick={() => openModal(transaction)}
+                                        >
+                                            <IconInfoCircle size={32} />
+                                        </ActionIcon>
+                                    </td>
+                                )}
+
+                            </tr>
+
+                        ))}
+                    </tbody>
+
+                </Table>
             )}
         </>
     );
